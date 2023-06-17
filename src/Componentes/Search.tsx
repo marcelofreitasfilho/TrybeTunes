ac961b696d41c41f478c43dbecd11423a65633b9
marcelofreitasfilho <marcelodefreitasfilho03@gmail.com>
2023-06-17 17:08:13 -0300

    req 2, teste 02

--- src/Componentes/Search.tsx
+++ src/Componentes/Search.tsx
@@ -1,4 +1,19 @@
+import { useState, ChangeEvent } from 'react';
+
 export function Search() {
+// ----------------------ESTADOS--------------------------------
+  const [btnState, setBtnState] = useState(false);
+  const [inptLog, setInpt] = useState('');
+
+  // ----------------------FUNCOES--------------------------------
+  const handleBTNChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const artistaBusca = e.target.value;
+    setInpt(artistaBusca);
+    if (artistaBusca.length >= 2) {
+      setBtnState(true);
+    }
+  };
+
   return (
     <form action="">
       <label htmlFor="busca">Busca</label>
@@ -7,9 +22,15 @@ export function Search() {
         name=""
         id="busca"
         data-testid="search-artist-input"
+        value={ inptLog }
+        onChange={ handleBTNChange }
       />
 
-      <button data-testid="search-artist-button">
+      <button
+        type="submit"
+        data-testid="search-artist-button"
+        disabled={ !btnState }
+      >
         Pesquisar
       </button>
     </form>
